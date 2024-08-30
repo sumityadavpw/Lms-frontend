cb44547e13a71af5518827f7ee76fe695d08a12d
sumityadavpw <sumityadav9521@gmail.com>
2024-08-31 00:54:24 +0530

    Added complete signup flow

--- Lms-frontend/src/App.jsx
+++ Lms-frontend/src/App.jsx
@@ -5,6 +5,7 @@ import { Route, Routes } from 'react-router-dom';
 import AboutUs from './Pages/AboutUs';
 import Homepage from './Pages/HomePage';
 import NotFound from './Pages/NotFound';
+import Signup from './Pages/Signup';
 
 function App() {
 
@@ -13,6 +14,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Homepage />}></Route> | 
         <Route path= "/about" element={<AboutUs/>} ></Route>
+
+        <Route path="/signup" element={<Signup />}/>
 
         <Route path="*" element={<NotFound/>}></Route>
       </Routes>
