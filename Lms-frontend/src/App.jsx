446077e40fb9bbc8623fb6da9de1b485d3ef8d9b
sumityadavpw <sumityadav9521@gmail.com>
2024-09-20 23:34:47 +0530

    Added edit profile page

--- Lms-frontend/src/App.jsx
+++ Lms-frontend/src/App.jsx
@@ -14,6 +14,7 @@ import CourseDescription from './Pages/Course/CourseDescription';
 import RequireAuth from './Components/Auth/RequireAuth';
 import CreateCourse from './Pages/Course/CreateCourse';
 import Profile from './Pages/User/Profile';
+import EditProfile from './Pages/User/EditProfile';
 function App() {
 
   return (
@@ -36,6 +37,7 @@ function App() {
 
         <Route element={<RequireAuth allowedRoles={["ADMIN", "USER"]} />}>
           <Route path='/user/profile' element={<Profile />} /> 
+          <Route path='/user/editprofile' element={<EditProfile/>} />
         </Route>
         <Route path="*" element={<NotFound />}></Route>
       </Routes>
