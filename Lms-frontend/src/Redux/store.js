e2ad102024c0b26a49fbbdd6fb40c76030cc4447
sumityadavpw <sumityadav9521@gmail.com>
2024-09-27 22:35:35 +0530

    Added complete checkout success logic

--- Lms-frontend/src/Redux/store.js
+++ Lms-frontend/src/Redux/store.js
@@ -2,10 +2,13 @@ import { configureStore } from "@reduxjs/toolkit";
 
 import authSliceReducer from './Slices/AuthSlice';
 import CourseSliceReducer from './Slices/CourseSlice';
+import razorpaySliceReducer from './Slices/RazorpaySlice';
+
 const store = configureStore({
     reducer: {
         auth: authSliceReducer,
-        course: CourseSliceReducer
+        course: CourseSliceReducer,
+        razorpay: razorpaySliceReducer
     },
     devTools: true
 });
