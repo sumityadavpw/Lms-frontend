0327894efbe727487881b59c124acf30ccf52452
sumityadavpw <sumityadav9521@gmail.com>
2024-10-01 00:39:58 +0530

    Added lecture slice

--- Lms-frontend/src/Redux/store.js
+++ Lms-frontend/src/Redux/store.js
@@ -3,12 +3,15 @@ import { configureStore } from "@reduxjs/toolkit";
 import authSliceReducer from './Slices/AuthSlice';
 import CourseSliceReducer from './Slices/CourseSlice';
 import razorpaySliceReducer from './Slices/RazorpaySlice';
+import lectureSliceReducer from "./Slices/LectureSlice";
+
 
 const store = configureStore({
     reducer: {
         auth: authSliceReducer,
         course: CourseSliceReducer,
-        razorpay: razorpaySliceReducer
+        razorpay: razorpaySliceReducer,
+        lecture: lectureSliceReducer
     },
     devTools: true
 });
