c0988436abf3db85d9083d3703091cbf6e5920ac
sumityadavpw <sumityadav9521@gmail.com>
2024-10-02 02:20:19 +0530

    Added course lectures

--- Lms-frontend/src/App.jsx
+++ Lms-frontend/src/App.jsx
@@ -18,6 +18,7 @@ import EditProfile from './Pages/User/EditProfile';
 import Checkout from './Pages/Payment/Checkout';
 import CheckoutSuccess from './Pages/Payment/CheckoutSuccess';
 import CheckoutFailure from './Pages/Payment/CheckoutFailure';
+import Displaylectures from './Pages/Dashboard/Displaylectures';
 function App() {
 
   return (
@@ -44,9 +45,7 @@ function App() {
           <Route path='checkout' element={<Checkout/>}/>
           <Route path='checkout/success' element={<CheckoutSuccess/>}/>
           <Route path='checkout/fail' element={<CheckoutFailure/>}/>
-
-          
-
+          <Route path='/course/displaylectures' element={<Displaylectures/>}/>
         </Route>
         <Route path="*" element={<NotFound />}></Route>
       </Routes>
